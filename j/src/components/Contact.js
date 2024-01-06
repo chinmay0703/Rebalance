8dad8563b4349fae08a612872a5021b3c6e54c51
Chinmay Satish Vyawahare <chinya2103@gmail.com>
2024-01-06 12:12:24 +0530

    finaly done

--- j/src/components/Contact.js
+++ j/src/components/Contact.js
@@ -61,7 +61,17 @@ const Contact = () => {
         <div className="container">
           <div className="footer-cta pt-5 pb-5">
             <div className="row">
-              <div className="col-xl-4 col-md-4 mb-30">
+              {/* <div className="col-xl-5 col-md-5 mb-30">
+                <div className="single-cta">
+                  <a href='https://maps.app.goo.gl/5NPMoY3nmSbquKcg8'>  <i className="fas fa-map-marker-alt"></i></a>
+                  <div className="cta-text">
+                    <h4 className='mx-2'>Find us</h4>
+                    <span className='mx-1'>610, 2nd floor, AECS Layout C Block <br></br> AECS Layout Whitefield, Bangalore</span>
+
+                  </div>
+                </div>
+              </div> */}
+              {/* <div className="col-xl-4 col-md-4 mb-30">
                 <div className="single-cta">
                   <div className="cta-text">
                     <i className="fas fa-map-marker-alt" style={{ verticalAlign: 'middle' }}></i>
@@ -69,23 +79,46 @@ const Contact = () => {
                     <span style={{ display: 'block', marginTop: '5px' }}>610, 2nd floor, AECS Layout - C Block, AECS Layout, Brookefield, Bengaluru, Karnataka 560037</span>
                   </div>
                 </div>
-              </div>
-
-              <div className="col-xl-4 col-md-4 mb-30">
+              </div> */}
+              <div className="col-xl-5 col-md-5 mb-30 ">
                 <div className="single-cta">
-                  <i className="fas fa-phone"></i>
+                  <div className='col-2'>
+                    <i className="fas fa-map-marker-alt my-3"></i>
+                  </div>
                   <div className="cta-text">
-                    <h4>Call us</h4>
-                    <span>+7721099005</span>
+                    <div className='col'>
+                      <h4 className='mx-1'>Find us</h4>
+                      <span className=''>610, 2nd floor, AECS Layout - C Block, </span><br></br><span className='mx-'>AECS Layout, Brookefield,</span>
+                      <br></br><span>Bengaluru, Karnataka 560037</span>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="col-xl-4 col-md-4 mb-30">
+
+              <div className="col-xl-3 col-md-3 mb-30 ">
                 <div className="single-cta">
-                  <i className="far fa-envelope-open"></i>
+                  <div className='col-1'>
+                    <i className="fas fa-phone my-3"></i>
+                  </div>
                   <div className="cta-text">
-                    <h4>Mail us</h4>
-                    <span>rebalance@gmail.com</span>
+                    <div className='col-9'>
+                      <h4>Call us</h4>
+                      <span>+7721099005</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='col-1'></div>
+              <div className="col-xl-2 col-md-2 mb-30">
+                <div className="single-cta">
+                  <div className='col-6'>
+                    <i className="far fa-envelope-open my-3"></i>
+                  </div>
+                  <div className="cta-text">
+                    <div className='col-6'>
+                      <h4>Mail us</h4>
+                      <span>rebalance@gmail.com</span>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -120,11 +153,9 @@ const Contact = () => {
                     <li><a href="#">ContactUs</a></li>
                     <li><a href="/gallery">Gallery</a></li>
                     <li><a href="/aboutus">About us</a></li>
-
                   </ul>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
