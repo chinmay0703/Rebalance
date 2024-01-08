bb115f46f184ff8a925097342ea1a3c4f0f8cacc
Chinmay Satish Vyawahare <chinya2103@gmail.com>
2024-01-08 14:59:13 +0530

    w

--- j/src/components/Advanced.js
+++ j/src/components/Advanced.js
@@ -1,6 +1,6 @@
 import './clinic.css';
 import React, { useRef, useEffect, useState } from 'react';
-import { Carousel, Card, Row, Col } from 'react-bootstrap';
+import { Carousel, Card } from 'react-bootstrap';
 import pic1 from "../components/images/physiotherapist-helping-young-female-patient-her-clinic.jpg";
 import pic2 from "../components/images/medium-shot-doctor-checking-leg.jpg";
 import pic3 from "../components/images/masseur-massaging-shoulders-pregnant-woman.jpg";
@@ -33,9 +33,9 @@ const Advanced = () => {
 
   const images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8];
   const titles = [
-    'Physiotherapy',
+    'Pain management',
     'Orthopedic Checkup',
-    'Pregnancy Massage',
+    'Womens Health  ',
     'Osteopath Treatment',
     'Sports Injury',
     'Back Pain Relief',
@@ -43,55 +43,37 @@ const Advanced = () => {
     'Rehabilitation'
   ];
 
-  // Chunk the images, titles, and descriptions into groups of 1 for small screens and 3 for large screens
-  const chunkedItems = images.reduce((resultArray, item, index) => {
-    const chunkIndex = Math.floor(index / 3);
-
-    if (!resultArray[chunkIndex]) {
-      resultArray[chunkIndex] = [];
-    }
-
-    resultArray[chunkIndex].push({
-      image: item,
-      title: titles[index],
-      // description: descriptions[index]
-    });
-
-    return resultArray;
-  }, []);
+  const items = images.map((image, index) => ({
+    image,
+    title: titles[index],
+  }));
 
   return (
     <div className={`scrolling-componentty ${isVisible ? 'visible' : ''}`} ref={componentRef}>
       <h3>Multiple Treatment We Provide</h3>
-      <Carousel interval={4000} className="mx-6">
-        {chunkedItems.map((chunk, index) => (
-          <Carousel.Item key={index} className="py-1">
-            <Row>
-              {chunk.map(({ image, title, description }, subIndex) => (
-                <Col key={subIndex} sm={12} md={4} lg={4} className='my-3'>
-                  <Card className="faint-gray-card">
-                    <Card.Img variant="top" src={image} className="img-fluid" alt={`Image ${index * 3 + subIndex + 1}`} />
-                    <Card.Body className="d-flex flex-column justify-content-between">
-                      <div>
-                        <Card.Title className="text-center mb-3">{title}</Card.Title>
-                        <Card.Text className=''>
-
-                          <Link to={"https://www.eka.care/doctor/vaishnavi-tammewar-orthopedic-surgeon-bangalore/calendar"}>
-                            <p className="text-center">Consult Now →</p>
-                          </Link>
-                        </Card.Text>
-                      </div>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </Carousel.Item>
-        ))}
-      </Carousel>
-      <div style={{ padding: '10px', textAlign: 'center' }} className='colorga ' >
+      <div className="d-flex justify-content-center align-items-center vh-10">
+        <Carousel interval={3000} className="mx-6 my-6">
+          {items.map((item, index) => (
+            <Carousel.Item key={index} className="py-1">
+              <Card className="faint-gray-card">
+                <Card.Img variant="top" src={item.image} className="img-fluid" alt={`Image ${index + 1}`} />
+                <Card.Body className="d-flex flex-column justify-content-between">
+                  <div>
+                    <Card.Title className="text-center mb-3">{item.title}</Card.Title>
+                    <Card.Text className=''>
+                      <Link to={"https://www.eka.care/doctor/vaishnavi-tammewar-orthopedic-surgeon-bangalore/calendar"}>
+                        <p className="text-center">Consult Now →</p>
+                      </Link>
+                    </Card.Text>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+      <div style={{ padding: '10px', textAlign: 'center' }} className='colorga '>
         <h1 className='text-center colorrr my-3 upper'><b>Take a step towards rebalancing your body</b></h1><br></br>
-       
       </div>
     </div>
   );
