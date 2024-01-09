0b40c9f5f58df748e19e287ab17e2a633b6dc1c5
Chinmay Satish Vyawahare <chinya2103@gmail.com>
2024-01-09 13:07:27 +0530

    j

--- j/src/components/Imagecolumn.js
+++ j/src/components/Imagecolumn.js
@@ -23,16 +23,16 @@ const Imagecolumn = () => {
             <h1 class="display text-center card-title my-3">Our expertise</h1>
             {/* <h3>The physiotherapy treatments on offer at Rebalance include but are not limited to:</h3> */}
             <ul class="list-group card-body">
-              <li class="list-group-item">1) Spinal Injuries (Disc, Muscles)</li>
-              <li class="list-group-item">2) Sports Injuries</li>
-              <li class="list-group-item">3) Postural related Injuries</li>
-              <li class="list-group-item">4) Post Surgery Rehabilitation (Hip/Knee replacement, ACL repair etc)</li>
-              <li class="list-group-item">5) Neurological Rehab (Stroke, MS, Parkinsons)</li>
-              <li class="list-group-item">6) Manual Therapy</li>
-              <li class="list-group-item">7) Specialized late-stage rehab programs</li>
-              <li class="list-group-item">8) Strength and power testing for return to play</li>
-              <li class="list-group-item">9) Individually tailored return to play programs</li>
-              <li class="list-group-item">10) Women's Health</li>
+              <li class="list-group-item mx-1">1) Spinal Injuries (Disc, Muscles)</li>
+              <li class="list-group-item mx-1">2) Sports Injuries</li>
+              <li class="list-group-item mx-1">3) Postural related Injuries</li>
+              <li class="list-group-item mx-1">4) Post Surgery Rehabilitation (Hip/Knee replacement, ACL repair etc)</li>
+              <li class="list-group-item mx-1">5) Neurological Rehab (Stroke, MS, Parkinsons)</li>
+              <li class="list-group-item mx-1">6) Manual Therapy</li>
+              <li class="list-group-item mx-1" >7) Specialized late-stage rehab programs</li>
+              <li class="list-group-item mx-1">8) Strength and power testing for return to play</li>
+              <li class="list-group-item mx-1">9) Individually tailored return to play programs</li>
+              <li class="list-group-item mx-1">10) Women's Health</li>
             </ul>
             <Link to={"https://www.eka.care/doctor/vaishnavi-tammewar-orthopedic-surgeon-bangalore/calendar"} >    <button class="btn btn-primary my-4 mx-5">Appointments</button></Link>
           </div>
