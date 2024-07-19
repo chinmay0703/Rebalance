a66364d52e8321443a61fe538e78f33e54c8bc35
chinmayIotreeminds <161325682+chinmayIotreeminds@users.noreply.github.com>
2024-07-19 18:40:43 +0530

    Updated the navbar

--- j/src/components/Aboutt.js
+++ j/src/components/Aboutt.js
@@ -8,77 +8,82 @@ import Map from './Map';
 import Contact from './Contact';
 import ChatBot from './ChatBox';
 function Aboutt() {
-  
-    const scrollToContact = () => {
-      scroller.scrollTo('contact', {
-        duration: 800,
-        smooth: true,
-      });
-    };
-    const scrollToTop = () => {
-      animateScroll.scrollToTop({
-        duration: 800,
-        smooth: true,
-  
-      });
-    };
-    return (
-      <div>
-        <header className='sticky-header'>
-         <nav class="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: " #e3f2fd" }}>
-           <div class="container-fluid">
-             <img
-               className='img-fluid '
-               src={pic}
-               alt="Your_image"
-               style={{ width: "120px" }}
-               onClick={scrollToTop}
-             />
-             <button class="navbar-toggler" style={{border:"none"}} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-             </button>
-             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                 <li class="nav-item">
-                   <b> <a class="nav-link active" aria-current="page" href="/" onClick={scrollToTop}>Home</a></b>
-                 </li>
-                 <li class="nav-item">
-                   <b> <a class="nav-link active" aria-current="page" href="/gallery">Gallery</a></b>
-                 </li>
-              
-                 <li class="nav-item">
-                   <b> <a class="nav-link active" aria-current="page" href="https://maps.app.goo.gl/5NPMoY3nmSbquKcg8">Location</a></b>
-                 </li>
-                 <li class="nav-item">
-                 <Nav.Link onClick={scrollToContact}><b>Contact Us</b></Nav.Link>
-                 </li>
-               </ul>
-  
-             </div>
-           </div>
-         </nav>
-       </header>
-        
-        <Clinic />
-        {/* <Tagline /> */}
-        <br></br>
-        <br></br>
-        {/* <br></br>
+
+  const scrollToContact = () => {
+    scroller.scrollTo('contact', {
+      duration: 800,
+      smooth: true,
+    });
+  };
+  const scrollToTop = () => {
+    animateScroll.scrollToTop({
+      duration: 800,
+      smooth: true,
+
+    });
+  };
+  return (
+    <div>
+      <header className='sticky-header'>
+        <nav class="navbar navbar-expand-lg navbar-light px-4 py-3" style={{ backgroundColor: "white" }}>
+          <div class="container-fluid">
+            <img
+              className='img-fluid '
+              src={pic}
+              alt="Your_image"
+              style={{ width: "150px" }}
+              onClick={scrollToTop}
+            />
+            <button class="navbar-toggler" style={{ border: "none" }} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-lg-0 p-2">
+                  <li class="nav-item">
+                    <a class="nav-link mx-1" aria-current="page" href="/" onClick={scrollToTop}><h6 class="nav-text" >Home</h6></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link mx-1" aria-current="page" href="/gallery"><h6 class="nav-text" >Gallery</h6></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link mx-1" aria-current="page" href="/aboutus" onClick={scrollToTop}><h6 class="nav-text" style={{ color: '#039dfc' }}>AboutUs</h6></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link mx-1" aria-current="page" href="https://maps.app.goo.gl/5NPMoY3nmSbquKcg8"><h6 class="nav-text">Location</h6></a>
+                  </li>
+                  <li class="nav-item" onClick={scrollToContact}>
+                    <a href="#" class="nav-link mx-1"><h6 class="nav-text">Contact Us</h6></a>
+                  </li>
+                </ul>
+              </div>
+
+
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <Clinic />
+      {/* <Tagline /> */}
+      <br></br>
+      <br></br>
+      {/* <br></br>
           <br></br> */}
-        <div className="about-us">
-          <center> <h1 className="animated-heading"><b>About Us</b></h1></center>
-          {/* Add the rest of your content here */}
-        </div>
-        <br></br>
-        <Aboutus ></Aboutus>
-        <br></br>
-        <Map></Map>
-        <br></br>
-        <Contact />
-        <ChatBot />
-  
-        <br></br>
+      <div className="about-us">
+        <center> <h1 className="animated-heading"><b>About Us</b></h1></center>
+        {/* Add the rest of your content here */}
       </div>
-    );
-  }
+      <br></br>
+      <Aboutus ></Aboutus>
+      <br></br>
+      <Map></Map>
+      <br></br>
+      <Contact />
+      <ChatBot />
+
+      <br></br>
+    </div>
+  );
+}
 export default Aboutt
